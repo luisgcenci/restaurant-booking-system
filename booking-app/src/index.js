81313026f3f12c2d93f9b243812ba452df42f29b
Kevin Hoang <kevinet19@gmail.com>
2021-03-10 18:50:06 +0000

    Delete index.js

--- booking-app/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './css/index.css';
-import App from './components/App';
-
-ReactDOM.render(
-  <App />, document.getElementById('root')
-);
-
