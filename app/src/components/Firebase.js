a4b5d1c9476788c071e1e666125b1ede87be15a1
luisgcenci <luisgcenci@gmail.com>
2021-03-23 12:34:53 -0500

    firebase set up

---
+++ app/src/components/Firebase.js
@@ -0,0 +1,29 @@
+import {useState} from 'react'
+
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import firebase from "firebase";
+// If you are using v7 or any earlier version of the JS SDK, you should import firebase using namespace import
+// import * as firebase from "firebase/app"
+
+// If you enabled Analytics in your project, add the Firebase SDK for Analytics
+// import "firebase/analytics";
+
+// // Add the Firebase products that you want to use
+// import "firebase/auth";
+// import "firebase/firestore";
+
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCqRvlZ2z93JJwkeV2AU1opSR_6y3srHAM",
+  authDomain: "booking-app-5f1de.firebaseapp.com",
+  projectId: "booking-app-5f1de",
+  storageBucket: "booking-app-5f1de.appspot.com",
+  messagingSenderId: "104953635657",
+  appId: "1:104953635657:web:b531c4d0bdf1152db17098",
+  measurementId: "G-ER090T9DKG"
+};
+
+var fire = firebase.initializeApp(firebaseConfig);
+
+export default fire;
--- app/src/components/Login.js
+++ app/src/components/Login.js
@@ -1,28 +1,44 @@
-import '../css/Login.css'
+import '../css/Login.css';
+import fire from './Firebase.js';
 
-import React from 'react';
+import React,{useState} from 'react';
 
 const Login = () => {
+
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+    const handleLogin = (e) => {
+
+        fire.database().ref('users/' + username).set({
+        username: username,
+        password: password
+    });
+        setUsername("");
+        setPassword(""); 
+    }
+
+
     return (
         <div className="Login container">
             <div className="row outer-row align-items-center justify-content-center">
                 <div className="col-12 main-col">
                         <div className="row form-row">
-                            <div className="form-inline">
+                            <form className="form-inline">
                                 <div className="form-group">
                                 <label htmlFor="" className ="col-3">Username</label>
                                 <div className="col-9">
-                                    <input type="text" placeholder = "Enter Username" className="form-control"/>
+                                    <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder = "Enter Username" className="form-control"/>
                                 </div>
                                 <label htmlFor="" className ="col-3">Password</label>
                                 <div className="col-9">
-                                    <input type="text" placeholder = "Enter Username" className="form-control"/>
+                                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder = "Enter Username" className="form-control"/>
                                 </div>
                                 <div className="col-9 offset-3">
-                                    <button type="submit">Log In</button>
+                                    <button onClick={handleLogin}type="submit">Log In</button>
                                 </div>
                                 </div>
-                            </div>
+                            </form>
                         </div>
                 </div>
             </div>
